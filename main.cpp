013116b5ea13f6202313c2d07ce8594595c10f11
edidada <sandisks555@gmail.com>
2019-02-14 23:25:11 +0800

    cause error

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <odb/database.hxx>
 #include <odb/transaction.hxx>
 
@@ -14,9 +13,9 @@ int main(int argc, char* argv[]) {
     try {
 //        db information is in argv
         auto_ptr<database> db(create_database(argc, argv));
-        student test("1","2",19);
+        student test("1","2",29);
         student make("5","4","3",19);
-        transaction t (db->begin ());
+        transaction t(db->begin ());
         db->persist(test);
         t.commit ();
     }catch (const odb::exception& e)
